873fb5ead4c71fe5979aba99365c76da5034f663
Vygantas Vilius <biesaz@gmail.com>
2024-03-13 20:16:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+query = {"age":{"$ne":40,"$ne":57}}
+
+    response = collection.find(query,{})
+    for document in response:
+        print(document)
+
+##########################################################
+        
+query = {"age":{"$gte":40,"$lte":57}}
+
+    response = collection.find(query,{})
+    for document in response:
+        print(document)
+
+##########################################################
+
+collection = get_database_collection(db, collection_name)
+    age = [40, 50, 57]
+    query = {"age":{"$nin":age}}
+
+    response = collection.find(query,{})
+    for document in response:
+        print(document)
